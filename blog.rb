18f12666b054cc61603b1882e195707a6304cec2
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-27 18:26:15 +0000

    Blog class

---
+++ blog.rb
@@ -0,0 +1,18 @@
+class Blog
+  attr_accessor :posts
+  def initialize
+    @posts = {}
+  end
+  
+  def create_post
+  end
+  
+  def read_post
+  end
+  
+  def update_post
+  end
+  
+  def delete_post
+  end
+end
